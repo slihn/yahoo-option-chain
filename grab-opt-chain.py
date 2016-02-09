db7300774a635aa833531df0c36b673cfba39ed7
Stephen Lihn <stevelihn@gmail.com>
2016-02-08 22:36:48 -0500

    uses CBOE-like format for elliptic pricing model

--- grab-opt-chain.py
+++ grab-opt-chain.py
@@ -6,6 +6,7 @@ from lxml import html
 from time import gmtime, strftime
 import logging
 import sys
+import time
 
 
 logger = logging.getLogger(__name__)
@@ -27,65 +28,88 @@ def get_options_menu(symbol: str, dt: int=None):
     options_menu = quote_table.find(".//div[@id='options_menu']/form")
     # etree.dump(options_menu)
     menu = dict()
-    for exp in options_menu.findall(".//option"):
-        exp_ms = int(float(exp.get("value")))
-        exp_dt = gmtime(exp_ms)
-        exp_dt_iso = strftime("%Y-%m-%d", exp_dt)
-        exp_tm_iso = strftime("%H:%M:%S", exp_dt)
-        assert exp_tm_iso == "00:00:00", "Expiration assertion failed: %d %s %s" % (exp_ms, exp_dt_iso, exp_tm_iso)
-        menu[exp_ms] = exp_dt_iso
-        logger.debug("Expiry %d: %s" % (exp_ms, exp_dt_iso))
+    for expiry in options_menu.findall(".//option"):
+        expiry_ms = int(float(expiry.get("value")))  # epoch time
+        expiry_dt = gmtime(expiry_ms)
+        expiry_dt_iso = strftime("%Y-%m-%d", expiry_dt)
+        expiry_tm_iso = strftime("%H:%M:%S", expiry_dt)
+        assert expiry_tm_iso == "00:00:00", \
+            "Expiration assertion failed: %d %s %s" % (expiry_ms, expiry_dt_iso, expiry_tm_iso)
+        menu[expiry_ms] = expiry_dt_iso
+        logger.debug("Expiry %d: %s" % (expiry_ms, expiry_dt_iso))
 
     return menu
 
 
-def get_option_chain(symbol: str, dt: int=None):
-    tree = fetch_from_yahoo(symbol, dt)
+def get_option_chain(symbol: str, expiry_ms: int=None):
+    tree = fetch_from_yahoo(symbol, expiry_ms)
     # <span id="yfs_l84_^XDE" data-sq="^XDE:value">109.00</span>
     value_elem = tree.find(".//span[@data-sq='%s:value']" % symbol)
     assert symbol in value_elem.get("id"), "Failed to locate price element"
-    price = value_elem.text # UNDL_PRC
+    price = value_elem.text  # UNDL_PRC
 
     # <div id="quote-table">
     quote_table = tree.find(".//div[@id='quote-table']")
     # <table class="details-table quote-table Fz-m">
+
+    out = ""
     for data_table in quote_table.findall(".//table"):
         # etree.dump(data_table)
         for tr in data_table.findall(".//tr"):
+            data = dict()
             row_id = tr.get("data-row")
-            if row_id is not None:
-                row_id = int(row_id)
-                row = [td.xpath("normalize-space()") for td in tr.findall("./td")]
-                if not row[1].startswith(symbol.strip('^')):
-                    continue
-                contract = row[1]
-                put_call = contract[len(symbol)+6-1]
-                assert put_call=="P" or put_call== "C", "put_call is not P,C"
-                row[6] = "%4.2f" % p2f(row[6])
-                row[9] = "%4.2f" % p2f(row[9])
-                print("%2d,%s,%s," % (row_id, price, put_call), ",".join(row))
+            if row_id is None:
+                continue
 
-                # TRADE_DT
-                # 0: Strike STRK_PRC, Contract Name,
-                # 2: Last, Bid, Ask, LAST,    L_BID,    L_ASK
-                # 5: Change, %Change, Volume, VOL
-                # 8: Open Interest, Implied Volatility
+            row_id = int(row_id)
+            row = [td.xpath("normalize-space()") for td in tr.findall("./td")]
+            if not row[1].startswith(symbol.strip('^')):
+                continue
 
-                # CBOE Style
-                # TRADE_DT, UNDLY, CLS,  EXPR_DT, STRK_PRC, PC,    OIT,   VOL,  HIGH,   LOW,  OPEN,  LAST,    L_BID,    L_ASK, UNDL_PRC,  S_TYPE,P_TYPE
-                # 20150601,   SPX, SPX, 20150619, 100.0000,  C, 2.0000,0.0000,0.0000,0.0000,0.0000,0.0000,2006.8000,2011.1000,2111.7300,Standard,Index
+            # row structure:
+            # 0: Strike STRK_PRC, 1: Contract Name,
+            # 2: Last, 3: Bid, 4: Ask, 5: LAST, 6: L_BID, 7: L_ASK
+            # 5: Change, 6: %Change, 7: Volume
+            # 8: Open Interest, 9: Implied Volatility
 
-    return
+            data['ROW'] = "%2d" % row_id
+            data['TRADE_DT'] = time.strftime("%Y%m%d")
+
+            ls = len(symbol)
+            if symbol[0] == '^':
+                ls -= 1
+
+            contract = row[1]  # Contract Name
+            put_call = contract[ls+6]
+            assert put_call=="P" or put_call== "C", "put_call is not P,C"
+
+            data['EXPR_DT'] = "20%s" % contract[ls:ls+6]
+            data['UNDL_PRC'] = price
+            data['PC'] = put_call
+            data['STRK_PRC'] = row[0]
+            data['OPT_SYMBOL'] = contract
+            data['LAST'] = row[2]
+            data['L_BID'] = row[3]
+            data['L_ASK'] = row[4]
+            data['CHANGE'] = row[5]
+            data['PCT_CHANGE'] = "%4.2f" % p2f(row[6])
+            data['VOL'] = row[7]
+            data['OIT'] = row[8]
+            data['IVOL'] = "%4.2f" % p2f(row[9])
+
+            out += ','.join([data[k] for k in headings()]) + "\n"
+
+    return out
 
 
 def p2f(x):
     return float(x.strip('%'))/100
 
 
-def fetch_from_yahoo(symbol, dt):
+def fetch_from_yahoo(symbol, expiry_ms):
     url = "http://finance.yahoo.com/q/op?s=%s+Options" % symbol
-    if dt is not None:
-        url = url + "&date=" + str(dt)
+    if expiry_ms is not None:
+        url = url + "&date=" + str(expiry_ms)
 
     logger.info("Fetch data from %s" % url)
     res = request.urlopen(url)
@@ -98,6 +122,22 @@ def fetch_from_yahoo(symbol, dt):
     return tree
 
 
-symbol = "^XDE"
-get_options_menu(symbol)
-get_option_chain(symbol)
+def get_xde_data():
+    out = ','.join(headings()) + "\n"
+    symbol = "^XDE"
+    expiry_menu = get_options_menu(symbol)
+    for expiry_ms in expiry_menu.keys():
+        out += get_option_chain(symbol, expiry_ms)
+    return out
+
+
+def headings():
+    return ['ROW', 'TRADE_DT', 'EXPR_DT', 'UNDL_PRC', 'PC', 'STRK_PRC',
+            'OPT_SYMBOL', 'LAST', 'L_BID', 'L_ASK', 'CHANGE', 'PCT_CHANGE', 'VOL', 'OIT', 'IVOL']
+
+
+# ------------------------------------------
+if __name__ == "__main__":
+    print(get_xde_data())
+
+# end of main
